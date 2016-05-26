4509955257342cabcada3e5cf827ecb63794ab5b
Josh <you@example.coexit>
2016-05-25 20:25:47 -0500

    prev and next functionality

--- scripts/main.js
+++ scripts/main.js
@@ -72,6 +72,98 @@ function search(){
 	);
 }
 
+// Next Page Function
+function nextPage(){
+	var token = $('#next-button').data('token');
+	var q = $('#next-button').data('query');
+
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	// Get Form Input
+	q = $('#query').val();
+
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type: 'video',
+			key: 'AIzaSyAdwayPNWc5ogk_rfpiGN9FFZ1CxWwYifM'},
+			function(data) {
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+
+				// Log Data
+				console.log(data);
+
+				$.each(data.items, function (i, item){
+					// Get Output
+					var output = getOutput (item);
+
+					// Display Results
+					$('#results').append(output);
+				});
+
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+
+				// Display Buttons
+				$('#buttons').append(buttons);
+
+			
+		}
+	);
+}
+
+// Previous Page Function
+function prevPage(){
+	var token = $('#prev-button').data('token');
+	var q = $('#prev-button').data('query');
+
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	// Get Form Input
+	q = $('#query').val();
+
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type: 'video',
+			key: 'AIzaSyAdwayPNWc5ogk_rfpiGN9FFZ1CxWwYifM'},
+			function(data) {
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+
+				// Log Data
+				console.log(data);
+
+				$.each(data.items, function (i, item){
+					// Get Output
+					var output = getOutput (item);
+
+					// Display Results
+					$('#results').append(output);
+				});
+
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+
+				// Display Buttons
+				$('#buttons').append(buttons);
+
+			
+		}
+	);
+}
+
 // Build Output
 function getOutput(item){
 	var videoId = item.id.videoId;
